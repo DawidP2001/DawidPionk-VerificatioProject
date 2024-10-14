c28c4d87472f771d873811309e7784f271b36c74
DawidP2001 <c00273530@setu.ie>
2024-10-14 11:18:14 +0100

    Created the unit testing class and simple layout

---
+++ PionkDawidTestTask1.java
@@ -0,0 +1,24 @@
+//Author:  DawidPionk
+//Date:    14/10/2024
+//Purpose: Task 1 Ver Project
+
+//package cm;
+
+import org.junit.Test;
+
+public class PionkDawidTestTask1 {
+    public class RateTest{
+        @Test
+        public void testRate(){
+
+        }
+        @Test
+        public void testCalculate(){
+
+        }
+        
+    }
+    public class PeriodTest{
+
+    }
+}
